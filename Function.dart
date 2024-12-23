a3ee83a553827929b07326011828da897f7d8a86
Ayush Karun <ayushkarun2580@gmail.com>
2024-12-23 12:26:57 +0530

    Dart Exercise

---
+++ Function.dart
@@ -0,0 +1,26 @@
+void main()
+{
+perimeter();
+}
+
+void perimeter(){
+int length =3;
+int breadth=2;
+int perimeter=2*(length+breadth);
+print("Perimeter is $perimeter");
+}
+/*
+////Another Way
+
+void main()
+{
+perimeter(4,2);
+}
+
+void perimeter(int length,int breadth)
+{
+int primeter=2*(length+breadth);
+print("Perimeter is $perimeter");
+}
+
+*/
---
+++ Function_with_return.dart
@@ -0,0 +1,11 @@
+void main()
+{
+int result=perimeter(4,2);
+print("Result is $result");
+}
+
+int perimeter(int length,int breadth)
+{
+int perimeter=2*(length+breadth);
+return perimeter;
+}
