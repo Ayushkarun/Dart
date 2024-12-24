f884fdb8317ad56f84a707affcecf561a02f883a
Ayush Karun <ayushkarun2580@gmail.com>
2024-12-24 13:03:00 +0530

    Dart exercise

---
+++ FunctionWithout_argument_return.dart
@@ -0,0 +1,54 @@
+////Print "Dart is Fun" 5 Times
+void dart()
+{
+  for(int i=1;i<=5;i++)
+  {
+    print("Dart is fun");
+  }
+}
+
+void main()
+{
+  dart();
+}
+/*
+
+////Print Multiplication Table
+
+void printTableOf5()
+{
+  int a=5;
+  for (int i = 1; i <= 5; i++)
+  {
+    print("$a * $i =  ${a*i}");
+  }
+}
+void main()
+{
+  printTableOf5();
+}
+
+
+////Display Current Date
+void printdate()
+{
+  var curr = DateTime.now();
+  print("Today's date is: $curr");
+}
+
+void main()
+{
+  printdate();
+}
+////////////////////
+void Hello()
+{
+  print("Hello, welcome to Dart programming!");
+}
+
+void main()
+{
+  Hello();
+}
+
+*/
