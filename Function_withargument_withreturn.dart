514e54c8acf4268e7f6ebd0682d636342d13a984
Ayush Karun <ayushkarun2580@gmail.com>
2024-12-26 11:25:03 +0530

    Dart Exercise

---
+++ Function_withargument_withreturn.dart
@@ -0,0 +1,26 @@
+// Function to add two numbers
+int addNumbers(int a, int b) {
+  return a + b;
+}
+void main() {
+  int result = addNumbers(5, 3);
+  print("The sum is: $result"); 
+}
+
+/*
+// Function to check if a number is even or odd
+String checkEvenOdd(int number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+void main() {
+  int num = 7;
+  String result = checkEvenOdd(num);
+  print("$num is $result"); 
+}
+
+*/
