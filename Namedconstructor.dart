54200e344175eafe4324beaecff02f9e74842174
Ayush Karun <ayushkarun2580@gmail.com>
2025-01-22 15:22:07 +0530

    Namedconstructor

--- Namedconstructor.dart
+++ Namedconstructor.dart
@@ -22,3 +22,31 @@ void main() {
   var car2 = Car.newModel('Tesla');
   car2.displayDetails();
 }
+
+
+/*
+
+class A {
+  String? name;
+  int? age;
+
+  A(this.name, this.age);
+
+  A.ayush(this.name) {
+    age = 13;
+  }
+
+  void result() {
+    print("Result is $name and age is $age");
+  }
+}
+
+void main() {
+  A ak = A("AYUSH", 23);
+  ak.result();
+
+  A ck = A.ayush("CK");
+  ck.result();
+}
+
+*/
