a042676c92153504cd336ad2ef66da4b91ae2503
Ayush Karun <ayushkarun2580@gmail.com>
2025-01-30 12:04:19 +0530

    Update reverse_list.dart

--- reverse_list.dart
+++ reverse_list.dart
@@ -1,10 +1,8 @@
 void main() {
-  // Original list
   List<int> numbers = [1, 2, 3, 4, 5];
   
   print("Original List: $numbers");
 
-  // Reversing the list
   List<int> reversedNumbers = numbers.reversed.toList();
   
   print("Reversed List: $reversedNumbers");
